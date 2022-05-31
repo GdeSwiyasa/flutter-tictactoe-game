6ee8d55be236fa38a02e6eaad984f32d448658d1
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-05-31 08:59:53 +0800

    create custom color for app

---
+++ lib/common/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  Color kPrimaryColor = Color(0xff00061A);
+  Color kSecondaryColor = Color(0xff001456);
+  Color kAccentColor = Color(0xff4169e8);
+}
