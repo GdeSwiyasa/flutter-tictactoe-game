3e9c07e3234067a73d382d2ffc474aa24193bfc6
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-05-31 09:00:27 +0800

    create main screen to view home game

---
+++ lib/screen/main_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe_game/common/theme.dart';
+import 'package:tictactoe_game/controller/logic.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  String lastValue = 'X';
+  bool gameOver = false;
+  int turn = 0;
+  String result = '';
+  List<int> scoreBoard = [0, 0, 0, 0, 0, 0, 0, 0];
+
+  Game game = Game();
+
+  @override
+  void initState() {
+    super.initState();
+    game.board = Game.initGameBoard();
+    print(game.board);
+  }
+
+  Widget build(BuildContext context) {
+    double boardWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: CustomColor().kPrimaryColor,
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Its ${lastValue} turn'.toUpperCase(),
+            style: TextStyle(color: Colors.white, fontSize: 58),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: boardWidth,
+            height: boardWidth,
+            child: GridView.count(
+              crossAxisCount: Game.boardLenth ~/ 3,
+              padding: EdgeInsets.all(16.0),
+              mainAxisSpacing: 8.0,
+              crossAxisSpacing: 8.0,
+              children: List.generate(Game.boardLenth, (index) {
+                return InkWell(
+                  onTap: gameOver
+                      ? null
+                      : () {
+                          if (game.board![index] == '') {
+                            setState(() {
+                              game.board![index] = lastValue;
+                              turn++;
+                              gameOver = game.winnerCheck(
+                                  lastValue, index, scoreBoard, 3);
+                              if (gameOver) {
+                                result = '$lastValue is the winner';
+                              }
+                              if (lastValue == 'X')
+                                lastValue = 'O';
+                              else
+                                lastValue = 'X';
+                            });
+                          }
+                        },
+                  child: Container(
+                    width: Game.blocSize,
+                    height: Game.blocSize,
+                    decoration: BoxDecoration(
+                      color: CustomColor().kSecondaryColor,
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: Center(
+                      child: Text(
+                        game.board![index],
+                        style: TextStyle(
+                          color: game.board![index] == 'X'
+                              ? Colors.blue
+                              : Colors.pink,
+                          fontSize: 64,
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            result,
+            style: TextStyle(color: Colors.white, fontSize: 54.0),
+          ),
+          ElevatedButton.icon(
+            onPressed: () {
+              setState(() {
+                game.board = Game.initGameBoard();
+                lastValue = 'X';
+                gameOver = false;
+                result = '';
+                turn = 0;
+                scoreBoard = [0, 0, 0, 0, 0, 0, 0, 0];
+              });
+            },
+            icon: Icon(Icons.replay),
+            label: Text("Repeat the Game"),
+          ),
+        ],
+      )),
+    );
+  }
+}
